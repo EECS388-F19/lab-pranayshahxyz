f4f6b1179e69da657825935984798cdacd42439b
pranays <pranays@umich.edu>
2019-09-06 16:05:16 -0400

    Lab 1

---
+++ helloworld.py
@@ -0,0 +1,14 @@
+import random
+
+print("Pranay Shah")
+
+rand1 = random.randint(0,101)
+rand2 = random.randint(0,101)
+print(rand1)
+print(rand2)
+
+sum = rand1 + rand2
+print("Sum = " + str(sum))
+
+avg = float(sum) / 2
+print("Average = " + str(avg))
